24a59352c211202495021e46f1b3f3644c5dbc2f
hhw <hhw50113@126.com>
2018-06-11 19:00:44 +0800

    added header file including all other headers

---
+++ stdops.h
@@ -0,0 +1,11 @@
+#include <string>
+#include <iostream>
+#include <map>
+#include <set>
+#include <cmath>
+#include "Node.h"
+#include "nodes.h"
+#include "Operator.h"
+#include "Ops.h"
+#include "func.h"
+
